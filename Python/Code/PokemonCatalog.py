ec4d7a46e54fe6cabda89b086b3b96c4f19f9cb1
Adrián <adrian08.1991@gmail.com>
2026-01-22 16:39:54 +0100

    Added alcremie

--- Python/Code/PokemonCatalog.py
+++ Python/Code/PokemonCatalog.py
@@ -80,6 +80,20 @@ pokemon_properties = {
         ],
     },
 
+    'alcremie': {
+    'skin': [
+        'caramel-swirl-berry', 'caramel-swirl-clover', 'caramel-swirl-flower', 'caramel-swirl-love', 'caramel-swirl-ribbon', 'caramel-swirl-star', 'caramel-swirl-strawberry',
+        'lemon-cream-berry', 'lemon-cream-clover', 'lemon-cream-flower', 'lemon-cream-love', 'lemon-cream-ribbon', 'lemon-cream-star', 'lemon-cream-strawberry',
+        'matcha-cream-berry', 'matcha-cream-clover', 'matcha-cream-flower', 'matcha-cream-love', 'matcha-cream-ribbon', 'matcha-cream-star', 'matcha-cream-strawberry',
+        'mint-cream-berry', 'mint-cream-clover', 'mint-cream-flower', 'mint-cream-love', 'mint-cream-ribbon', 'mint-cream-star', 'mint-cream-strawberry',
+        'rainbow-swirl-berry', 'rainbow-swirl-clover', 'rainbow-swirl-flower', 'rainbow-swirl-love', 'rainbow-swirl-ribbon', 'rainbow-swirl-star', 'rainbow-swirl-strawberry',
+        'ruby-cream-berry', 'ruby-cream-clover', 'ruby-cream-flower', 'ruby-cream-love', 'ruby-cream-ribbon', 'ruby-cream-star', 'ruby-cream-strawberry',
+        'ruby-swirl-berry', 'ruby-swirl-clover', 'ruby-swirl-flower', 'ruby-swirl-love', 'ruby-swirl-ribbon', 'ruby-swirl-star', 'ruby-swirl-strawberry',
+        'salted-cream-berry', 'salted-cream-clover', 'salted-cream-flower', 'salted-cream-love', 'salted-cream-ribbon', 'salted-cream-star', 'salted-cream-strawberry',
+        'vanilla-cream-berry', 'vanilla-cream-clover', 'vanilla-cream-flower', 'vanilla-cream-love', 'vanilla-cream-ribbon', 'vanilla-cream-star'
+        ],
+    },
+
     #region Region
     'raichu': {
         'skin': ['autumn', 'summer', 'winter'],
